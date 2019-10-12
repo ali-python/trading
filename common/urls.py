62b955220cedc9f27ba565eb00521caa8bd32fdf
Zaheer Jan <zaheerjan0@gmail.com>
2019-10-12 14:28:22 +0500

    index design fix, common urls update, product form, category form and product list update

--- common/urls.py
+++ common/urls.py
@@ -2,7 +2,7 @@ from .views import IndexView
 from django.urls import path
 
 urlpatterns = [
-    path('index/', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index')
 
 
 ]
--- partum_trading/urls.py
+++ partum_trading/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('common/', include(('common.urls', 'common'), namespace='common')),
+    path('', include(('common.urls', 'common'), namespace='common')),
     path('product/', include(('product.urls', 'product'), namespace='product')),
 
 ]
