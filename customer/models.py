de5af55ff0e4edf519d50c8598bfe68975165b29
Zaheer Jan <zaheerjanbadini@gmail.com>
2019-11-11 22:19:51 +0500

    reports and design fix with unpaid amount function in models

--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 from django.utils import timezone
 
 
@@ -15,6 +16,22 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_unpaid_amount(self):
+        customer_ledgers = self.customer_ledger.all()
+
+        if customer_ledgers:
+            ledger_debit = customer_ledgers.aggregate(Sum('debit_amount'))
+            ledger_credit = customer_ledgers.aggregate(Sum('credit_amount'))
+
+            debit_amount = ledger_debit.get('debit_amount__sum')
+            credit_amount = ledger_credit.get('credit_amount__sum')
+        else:
+            debit_amount = 0
+            credit_amount = 0
+
+        unpaid_amount = debit_amount - credit_amount
+        return unpaid_amount
 
 
 class CustomerLedger(models.Model):
